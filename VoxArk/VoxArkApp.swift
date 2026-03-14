ee48968f6c1c7fbc89cb9e76aefaba2b28f9f043
Yoshinori Ogawa <aiki8080@icloud.com>
2026-03-14 10:21:21 +0900

    Initial Commit

---
+++ VoxArk/VoxArkApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoxArkApp.swift
+//  VoxArk
+//
+//  Created by M4Mac(aiki8080) on 2026/03/14.
+//
+
+import SwiftUI
+
+@main
+struct VoxArkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
