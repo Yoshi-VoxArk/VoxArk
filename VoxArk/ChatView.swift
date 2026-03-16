f53398eba3713b838dae486508c3aed85ef2cf19
Yoshi-VoxArcAir <aiki8080@icloud.com>
2026-03-17 06:17:09 +0900

    完全ハンズフリー音声入力の追加とプレミアム音声の設定

---
+++ VoxArk/ChatView.swift
@@ -0,0 +1,18 @@
+//
+//  ChatView.swift
+//  VoxArk
+//
+//  Created by user on 2026/03/16.
+//
+
+import SwiftUI
+
+struct ChatView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChatView()
+}
