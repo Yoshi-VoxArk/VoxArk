3732b08cd8f8858449d516bd40456ec45ac3a607
Yoshi-VoxArcAir <aiki8080@icloud.com>
2026-03-17 15:49:39 +0900

    ConnectionManager.swiftを初実装

---
+++ VoxArk/ConnectionManager.swift
@@ -0,0 +1,18 @@
+//
+//  ConnectionManager.swift
+//  VoxArk
+//
+//  Created by user on 2026/03/17.
+//
+
+import SwiftUI
+
+struct ConnectionManager: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ConnectionManager()
+}
---
+++ VoxArk/ProfileSettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileSettingsView.swift
+//  VoxArk
+//
+//  Created by user on 2026/03/17.
+//
+
+import SwiftUI
+
+struct ProfileSettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileSettingsView()
+}
